93384f0f0ba2323e98e560c447260d46b4b4ac10
Rodolphe Gnamien <gnamienrodolphe9@gmail.com>
2026-04-09 14:08:44 +0000

    Ajout des nouvelles pages et styles du portfolio

---
+++ por.js
@@ -0,0 +1,63 @@
+const btn = document.getElementById("theme-btn");
+
+/* vérifier si un thème est déjà enregistré */
+if(localStorage.getItem("theme") === "dark"){
+document.body.classList.add("dark-mode");
+btn.textContent = "☀️";
+}
+
+btn.onclick = function(){
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+btn.textContent = "☀️";
+localStorage.setItem("theme","dark");
+}else{
+btn.textContent = "🌙";
+localStorage.setItem("theme","light");
+}
+};
+
+/* ANIMATION SCROLL */
+const containers = document.querySelectorAll(".container");
+const line = document.getElementById("line");
+
+window.addEventListener("scroll",()=>{
+
+let maxHeight = 0;
+
+containers.forEach(el=>{
+const top = el.getBoundingClientRect().top;
+const screen = window.innerHeight;
+
+if(top < screen - 100){
+el.style.opacity="1";
+el.style.transform="translateY(0)";
+maxHeight = el.offsetTop;
+}
+});
+
+line.style.height = maxHeight + "px";
+
+ });
+
+
+// const img = document.querySelector('.img');
+
+// window.addEventListener('')
+
+const images = document.querySelectorAll(".hero img, .carte img");
+
+window.addEventListener("scroll", () => {
+    images.forEach(img => {
+        const top = img.getBoundingClientRect().top;
+        const screen = window.innerHeight;
+
+        if (top < screen - 100) {
+            img.style.opacity = "1";
+            img.style.transform = "translate(0)";
+        }
+    });
+});
+
