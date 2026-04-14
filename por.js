289bfcbb5081f068477433e789b471f39f200e78
Rodolphe Gnamien <gnamienrodolphe9@gmail.com>
2026-04-14 13:49:44 +0000

    Modification de fichiers

--- por.js
+++ por.js
@@ -36,6 +36,7 @@ nav.classList.toggle("active");
 const containers = document.querySelectorAll(".container");
 const line = document.getElementById("line");
 
+
 window.addEventListener("scroll",()=>{
 
 let maxHeight = 0;
@@ -61,6 +62,7 @@ line.style.height = maxHeight + "px";
 // window.addEventListener('')
 
 const images = document.querySelectorAll(".hero img, .carte img");
+// const apropo = document.querySelectorAll(".apropo")
 
 window.addEventListener("scroll", () => {
     images.forEach(img => {
@@ -74,3 +76,60 @@ window.addEventListener("scroll", () => {
     });
 });
 
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+reveals.forEach(el => observer.observe(el));
+
+const texts = [
+    "Développeur Full Stack",
+    "Infographiste",
+    "Monteur Vidéo",
+    "UI/UX Designer",
+];
+
+let index = 0;   // Index du texte actuel mmot actuel dans le tableau
+let charIndex = 0;   // Index du caractère actuel position actuel dans le texte
+let isDeleting = false;
+ 
+const typingEL = document.querySelector(".typing");
+
+function typeEffect() {
+    let currentText = texts[index];
+
+    if (!isDeleting) {
+        charIndex++;
+        typingEL.textContent = currentText.substring(0, charIndex);
+    }else {
+        charIndex--;
+        typingEL.textContent = currentText.substring(0, charIndex);
+    }
+
+    let speed = isDeleting ? 50 : 100;
+
+    // Si le texte est complètement écrit, on attend un moment avant de commencer à supprimer
+    if (!isDeleting && charIndex === currentText.length) {
+        speed = 1500; // Pause avant de supprimer
+        isDeleting = true;
+    }
+
+    // Si le texte est complètement supprimé, on passe au texte suivant
+    else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        index = (index + 1) % texts.length; // Passe au texte suivant, revient au début si on atteint la fin
+    }
+
+    setTimeout(typeEffect, speed);
+}
+
+typeEffect();
